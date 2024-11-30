bfd50f65d16b01932f81c1137247f33bb23f681b
lazhar233 <lazhhar3@gmail.com>
2024-11-30 20:36:47 +0100

    edit1

--- app.js
+++ app.js
@@ -15,28 +15,18 @@ app.set('view engine', 'ejs');
 
 
 app.get('/',(req,res)=>{
-    // res.sendFile("./views/Login.html", {root: __dirname});
     res.render('hope', { } );
 })
 
-mongoose.connect("mongodb://lazhhar0001:Lazhar123@cluster2-shard-00-00.tkaoj.mongodb.net:27017,cluster2-shard-00-01.tkaoj.mongodb.net:27017,cluster2-shard-00-02.tkaoj.mongodb.net:27017/?ssl=true&replicaSet=atlas-tik33t-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster2")
-  .then(() => {
-        app.listen(port , () => {
-            console.log(`http://localhost:${port}/`)
-        }) ;
-        })
-   .catch((err) => {
-         console.log(err);
-        });
 
 
 
-        app.post("/", (req, res) => {
-            const Hope = new hope(req.body);
-          
-            Hope
-              .save()
-              .then( result => {
+app.post("/", (req, res) => {
+  const Hope = new hope(req.body);
+  
+  Hope
+  .save()
+  .then( result => {
                 res.redirect("/");
               })
               .catch( err => {
@@ -44,3 +34,22 @@ mongoose.connect("mongodb://lazhhar0001:Lazhar123@cluster2-shard-00-00.tkaoj.mon
               });
           });
 
+
+
+
+
+
+          mongoose.connect("mongodb://lazhhar0001:Lazhar123@cluster2-shard-00-00.tkaoj.mongodb.net:27017,cluster2-shard-00-01.tkaoj.mongodb.net:27017,cluster2-shard-00-02.tkaoj.mongodb.net:27017/?ssl=true&replicaSet=atlas-tik33t-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster2")
+            .then(() => {
+                  app.listen(port , () => {
+                      console.log(`http://localhost:${port}/`)
+                  }) ;
+                  })
+             .catch((err) => {
+                   console.log(err);
+                  });
+          
+          
+        // app.listen(port , () => {
+          //     console.log(`http://localhost:${port}/`)
+          // }) ;
