5c28c7442a4f032302dcdc17ee1c43fc03193005
lazhar233 <lazhhar3@gmail.com>
2024-11-30 15:45:50 +0100

    first commit

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 const hope = require("./models/articleSchema");
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json());
+app.set('view engine', 'ejs');
 
 
 
 
 app.get('/',(req,res)=>{
-    res.sendFile("./views/Login.html", {root: __dirname});
-    // res.render('index', { } );
+    // res.sendFile("./views/Login.html", {root: __dirname});
+    res.render('hope', { } );
 })
 
 mongoose.connect("mongodb://lazhhar0001:Lazhar123@cluster2-shard-00-00.tkaoj.mongodb.net:27017,cluster2-shard-00-01.tkaoj.mongodb.net:27017,cluster2-shard-00-02.tkaoj.mongodb.net:27017/?ssl=true&replicaSet=atlas-tik33t-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster2")
